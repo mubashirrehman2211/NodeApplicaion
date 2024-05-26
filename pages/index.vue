13b41240ffba99441ef9589ac0c8b5e42f0ba3ec
Mubashir Rehman <malikextra7@gmail.com>
2024-05-26 14:06:30 +0500

    get your current location & make Animated Loader for location and fetching data

--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,36 @@
 <script setup>
 import { useCounterStore } from "~/store/store.js";
-import USelect from "nvd-u/components/USelect.vue";
 import ULoading from "nvd-u/components/ULoading.vue";
+import DataCard from "~/components/DataCard.vue";
+import SearchInputs from "~/components/SearchInputs.vue";
 
 const store = useCounterStore();
 </script>
 
 <template>
-  <div class="bg-img absolute left-0 top-0 w-screen h-[90vh] bg-cover bg-fixed">
+  <div
+    class="bg-img absolute left-0 top-0 w-screen h-[90vh] bg-cover bg-center bg-fixed"
+  >
     <div class="absolute left-[30%] top-[25%]">
-      <h2 class="text-3xl font-semibold tracking-wide text-gray-900">
+      <h2 class="text-3xl font-semibold tracking-wide text-gray-50">
         Search the best Places Near You.
       </h2>
       <div class="flex flex-col mt-5">
-        <USelect
-          class="w-full"
-          :options="store.placesName"
-          label="select searching type"
-          value="value"
-          v-model="store.placeValue"
-        />
-
-        <USelect
-          class="w-full mt-10"
-          :options="store.citiesName"
-          label="Select city"
-          v-model="store.selectedValue"
-        />
-        <P class="text-red-400 mt-0" v-if="store.err">something went wrong</P>
-
-        <button
-          class="border-2 border-gray-600 font-semibold text-gray-200 px-10 h-12 mt-[2rem] bg-gray-600 rounded hover:bg-gray-400 hover:border-gray-400 hover:text-gray-700 duration-500"
-          @click="store.finalRequest()"
-        >
-          Search
-        </button>
+        <SearchInputs />
       </div>
       <div class="my-72 flex flex-col">
-        <TransitionGroup name="list">
-          <div
-            v-for="restaurant in store?.data"
-            :key="restaurant"
-            class="all-center flex-col border-2 border-gray-300 border-b-0 rounded cursor-pointer mt-5"
-          >
-            <div class="text-xl font-semibold text-gray-600 mt-2">
-              {{ restaurant?.title }}
-            </div>
-            <div class="text-xl font-semibold text-gray-600 my-2">
-              {{ restaurant?.avg_rating }} <span class="text-2xl">★</span>
-            </div>
-
-            <RouterLink :to="`/${restaurant?.title}`" class="w-full">
-              <button
-                class="border-2 border-gray-600 font-semibold text-white px-10 h-10 w-full bg-gray-600 rounded hover:bg-gray-400 hover:border-gray-400 hover:text-white duration-500"
-              >
-                Visit
-              </button>
-            </RouterLink>
-          </div>
-        </TransitionGroup>
-        <ULoading
-          v-show="store.loading"
-          class="all-center text-2xl mt-10"
-          v-if="!store?.data"
-        />
-
-        <div
-          class="text-lg my-10 all-center text-gray-300"
-          v-if="!store.loading || !store.data"
-        >
-          Your Search will appear here
-        </div>
+        <DataCard />
       </div>
     </div>
   </div>
+
+  <MainLoader v-if="store.locationLoader">
+    <img
+      src="/public/icons8-location-update.gif"
+      class="w-[5%] rounded-full"
+      alt="location-loader"
+    />
+  </MainLoader>
 </template>
 
 <style scoped>
--- store/store.js
+++ store/store.js
@@ -4,13 +4,13 @@ import axios from "axios";
 export const useCounterStore = defineStore("counter", {
   state: () => ({
     data: null,
+    mainLoader: false,
+    locationLoader: false,
     selectedRestaurant: null,
-    showBtn: false,
-    filterRestaurant: null,
+    filteredRestaurant: null,
     selectedValue: null,
-    errorMessage: "",
+    placeValue: null,
     err: false,
-    yourLocation: null,
     city: null,
     loading: false,
     placesName: [
@@ -58,54 +58,100 @@ export const useCounterStore = defineStore("counter", {
   getters: {},
 
   actions: {
+    currentLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (location) => {
+            const { latitude, longitude } = location.coords;
+
+            this.getLocationName(latitude, longitude);
+
+            this.locationLoader = true;
+
+            this.createNewCity();
+          },
+          (error) => {
+            console.log(error.message);
+          }
+        );
+      }
+    },
+
+    getLocationName(lat, lng) {
+      axios
+        .get(
+          "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+            lat +
+            "," +
+            lng +
+            "&key=AIzaSyC4800J442xrkb5zUzGSEHA5GHHnMmccgc"
+        )
+        .then((response) => {
+          if (response.data.error_message) {
+            this.error = response.data.error_message;
+          } else {
+            this.selectedValue = response.data.results[0].formatted_address;
+          }
+        })
+        .catch((error) => {
+          this.error = error.message;
+        });
+    },
+
+    createNewCity() {
+      setTimeout(() => {
+        this.cities.push({
+          name: this.selectedValue,
+          type: null,
+          lat: latitude,
+          lng: longitude,
+        });
+
+        this.locationLoader = false;
+      }, 3000);
+    },
+
+    findProduct() {
+      this.city = this.cities.find((item) => item.name === this.selectedValue);
+      this.city.type = this.placeValue;
+    },
+
+    clearDataAfterNewSearch() {
+      if (this.data) {
+        this.data = null;
+        this.mainLoader = true;
+      }
+    },
+
+    sendRequestToServer() {
+      this.mainLoader = true;
+      axios
+        .post("http://localhost:5555/sending", this.city)
+        .then((response) => {
+          this.data = response.data;
+          localStorage.setItem("data", JSON.stringify(this.data));
+          this.mainLoader = false;
+        });
+    },
+
     finalRequest() {
       if (this.selectedValue && this.placeValue) {
         this.findProduct();
-
+        console.log(this.city, "city");
         this.sendRequestToServer();
+        this.clearDataAfterNewSearch();
 
-        this.getDataFromServer();
         this.err = false;
       } else {
         this.err = true;
       }
     },
 
-    async findProduct() {
-      this.city = this.cities.find((item) => item.name === this.selectedValue);
-      this.city.type = this.placeValue;
-    },
-
-    sendRequestToServer() {
-      axios
-        .post("http://localhost:5555/sending", this.city)
-        .then((response) => (this.errorMessage = response.data.id));
-
-      // console.log(this.errorMessage, "err");
-    },
-
-    getDataFromServer() {
-      if (this.data) {
-        this.data = null;
-        this.loading = true;
-      }
-      // console.log(this.data, "Old data");
-
-      if (this.city) {
-        this.loading = true;
-        setTimeout(() => {
-          axios.get("http://localhost:5555/data").then((res) => {
-            this.data = res.data;
-            // console.log(this.data, "new data");
-          });
-        }, 6000);
-      }
-    },
-
     filterRestaurantDetail() {
       let route = useRoute();
       this.selectedRestaurant = route.params.id;
-      this.filterRestaurant = this.data?.find(
+      let data = JSON.parse(localStorage.getItem("data"));
+      this.filteredRestaurant = data?.find(
         (item) => item?.title === this?.selectedRestaurant
       );
     },
